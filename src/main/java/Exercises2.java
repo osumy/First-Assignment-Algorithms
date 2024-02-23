7605b180209060638b14836062457260dd898955
AmirAli Araghi <amirali40223@gmail.com>
2024-02-23 13:42:23 +0330

    twoSum

--- src/main/java/Exercises2.java
+++ src/main/java/Exercises2.java
@@ -14,7 +14,12 @@ public class Exercises2 {
     */
 
     public int[] twoSum(int[] nums, int target) {
-        // TODO
+        for (int i = 0; i < nums.length; i++){
+            for (int j = i+1; j < nums.length; j++){
+                if (nums[i] + nums[j] == target)
+                    return new int[]{i, j};
+            }
+        }
         return null;
     }
 
